ffd3dbbba636217feab6023ba870ede28be69d14
ayushs111 <sojitraayush301@gmail.com>
2022-03-03 09:08:20 +0530

    remove btn on dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,6 @@
-import React, { useState } from "react";
-import { Button } from "primereact/button";
+import React from "react";
 
 const Dashboard = () => {
-    const [show, setShow] = useState(true);
-    function action() {
-        if (show === true) {
-            setShow(false);
-        } else if (show === false) {
-            setShow(true);
-        }
-    }
-
-    // #1e1e1e
     return (
         <>
             <div className="grid">
@@ -60,27 +49,23 @@ const Dashboard = () => {
                         <span className="text-500">since last visit</span>
                     </div>
                 </div>
-                {show ? (
-                    <div className="col-12 lg:col-6 xl:col-3">
-                        <div className="card mb-0">
-                            <div className="flex justify-content-between mb-3">
-                                <div>
-                                    <span className="block text-500 font-medium mb-3">Orders</span>
-                                    <div className="text-900 font-medium text-xl">152</div>
-                                </div>
-                                <div className="flex align-items-center justify-content-center bg-blue-100 border-round" style={{ width: "2.5rem", height: "2.5rem" }}>
-                                    <i className="pi pi-shopping-cart text-blue-500 text-xl" />
-                                </div>
+
+                <div className="col-12 lg:col-6 xl:col-3">
+                    <div className="card mb-0">
+                        <div className="flex justify-content-between mb-3">
+                            <div>
+                                <span className="block text-500 font-medium mb-3">Orders</span>
+                                <div className="text-900 font-medium text-xl">152</div>
                             </div>
-                            <span className="text-green-500 font-medium">24 new </span>
-                            <span className="text-500">since last visit</span>
+                            <div className="flex align-items-center justify-content-center bg-blue-100 border-round" style={{ width: "2.5rem", height: "2.5rem" }}>
+                                <i className="pi pi-shopping-cart text-blue-500 text-xl" />
+                            </div>
                         </div>
+                        <span className="text-green-500 font-medium">24 new </span>
+                        <span className="text-500">since last visit</span>
                     </div>
-                ) : (
-                    <></>
-                )}
+                </div>
             </div>
-            <Button className="" label="show" onClick={() => action()} />
         </>
     );
 };
